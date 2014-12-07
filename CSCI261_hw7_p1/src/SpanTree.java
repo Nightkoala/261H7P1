716b5614154f13fa2387b61e2527dc7368f5f8ff
Derek Brown <djb3718@rit.edu>
2014-12-06 21:20:08 -0500

    Added repo to Eclipse and created java file

---
+++ CSCI261_hw7_p1/src/SpanTree.java
@@ -0,0 +1,23 @@
+/**
+ * SpanTree.java
+ * 
+ * @author	Derek Brown <djb3718@rit.edu>
+ *
+ * Purpose:	Finds the minimum f-containing spanning tree for any given weighted
+ * 			undirected graph.
+ */
+
+import java.util.Scanner;
+
+public class SpanTree {
+
+	// Attributes
+	
+	// Constructor
+	
+	// Methods
+	
+	public static void main(String[] args) {
+
+	}//end main
+}//end SpanTree class
